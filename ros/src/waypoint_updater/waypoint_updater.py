c326a053c337e13587100660c16a686377f8b016
laygond <laygond_bryan@hotmail.com>
2020-07-11 04:26:44 -0400

    :sparkles: partial implementation complete

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,8 +3,9 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
+import numpy as np
 import math
+from scipy.spatial import KDTree
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -37,16 +38,82 @@ class WaypointUpdater(object):
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None 
+        
+        self.loop() 
 
-        rospy.spin()
+        #rospy.spin()
+
+    def loop(self):
+        """
+        This Node's Main Loop: It runs forever at 50Hz until ROS is shutdown. 
+        """
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                # Get closest waypoint index
+                closest_waypoint_idx = self.get_closest_waypoint_idx()
+                # Publishes to final_waypoints from closest waypoint to closest waypoint + LOOKAHEAD_WPS
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep() 
+
+    def get_closest_waypoint_id(self):
+        """
+        Gets the closest waypoint index in front of the current car's pose. 
+        To determine if the closest waypoint is in front, a dot product is used as verification
+        """
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1] # first 1 is the closest item from (x,y), [0] refers to its distance, [1] refers to its index in tree
+        
+        # Check if closest is ahead or behind vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord    = self.waypoints_2d[closest_idx-1] 
+        
+        # Equation for hyperplane through closest_coords
+        cl_vect   = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect  = np.array([x, y])
+        
+        val= np.dot(cl_vect-prev_vect, pos_vect-cl_vect) 
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        
+        return closest_idx 
+
+    def publish_waypoints(self, closest_idx):
+        """
+        Creates a subset from base_waypoints from closest waypoint to closest waypoint + LOOKAHEAD_WPS.
+        Publishes as a Lane msg type the subset to final_waypoints topic.
+        """
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane) 
 
     def pose_cb(self, msg):
+        """
+        Callback function from subscriber to current_pose topic. It stores the current car's pose.
+        """
         # TODO: Implement
+        self.pose = msg
         pass
 
     def waypoints_cb(self, waypoints):
+        """
+        This is a special subscriber call back function that only runs once since input data never changes.
+        it stores the waypoints in base_waypoints and provides two 2D coordinate system alternatives.
+        waypoints_2D contains the (x,y) coordinates in a list and waypoint_tree in a KDTree data structure
+        for a faster search. 
+        """
         # TODO: Implement
-        pass
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
