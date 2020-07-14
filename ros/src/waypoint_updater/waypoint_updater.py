445f8004bd31be37a35b7fa7215757fde5bf5d60
laygond <laygond_bryan@hotmail.com>
2020-07-13 22:24:26 -0400

    :sparkles: addition of traffic calback function

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,21 +23,21 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+MAX_DECEL = .5      # [m/s2]
 
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
+        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        #rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.stopline_wp_idx = -1
         self.pose = None
         self.base_waypoints = None
         self.waypoints_2d = None
@@ -94,6 +94,34 @@ class WaypointUpdater(object):
         lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
         self.final_waypoints_pub.publish(lane) 
 
+    def generate_lane(sef):
+        lane - Lane() 
+        closest_idx = self.get_closest_waypoint_idx()
+        farthest_idx = closest_idx + LOOKAHEADWPS
+        roi_waypoints = self.base_waypoints.waypoints[closest idx:farthest_idx]
+        
+        if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
+            lane.waypoints = roi_waypoints
+        else:
+            lane.waypoints = self.decelerate_waypoints(roi_waypoints, closest_idx)
+        return lane 
+
+    def decelerate_waypoints(self, waypoints, closest_idx):
+        temp = []
+        for i, wp in enumerate(waypoints): 
+            p = Waypoint()
+            p.pose = wp.pose 
+            stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0) # two or three waypoints back from line so that the nose of the car stops before the line and not the center of car
+            dist = self.distance(waypoints, i, stop_idx)
+            vel = math.sqrt(2*MAX_DECEL*dist)
+            if vel < 1.:
+                vel = 0.
+            p.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
+            temp.append(p)
+            
+        return temp
+
+
     def pose_cb(self, msg):
         """
         Callback function from subscriber to current_pose topic. It stores the current car's pose.
@@ -117,7 +145,7 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        self.stopline_wp_idx = msg.data
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
