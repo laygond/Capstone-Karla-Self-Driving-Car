3e898f03582d17e96b80ac7f360dc9b10c0bdcc0
laygond <laygond_bryan@hotmail.com>
2020-08-02 19:19:26 +0000

    :bug: typo in init Conroller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,7 +20,7 @@ ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def init (self, vehicle_mass, fuel_capacity,
+    def __init__ (self, vehicle_mass, fuel_capacity,
              brake_deadband, decel_limit, accel_limit,
              wheel_radius, wheel_base,
              steer_ratio, max_lat_accel, max_steer_angle):
